87f27f4ac9299c90b021973580921736ec9afd31
podbor16 <podbor250718@gmail.com>
2026-05-19 03:43:13 +0700

    feat: подключить webhook router

--- src/tracker/router.py
+++ src/tracker/router.py
@@ -7,7 +7,9 @@ from fastapi import APIRouter
 
 from src.tracker.routers.pages import router as pages_router
 from src.tracker.routers.api import router as api_router
+from src.tracker.routers.webhook import router as webhook_router
 
 router = APIRouter(prefix="", tags=["tracker"])
 router.include_router(pages_router)
 router.include_router(api_router)
+router.include_router(webhook_router)
